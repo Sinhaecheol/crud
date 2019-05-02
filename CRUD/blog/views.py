66a6b4163d46ce7eb748683762b50e21f23392bd
Seungrae <37570657+Seungpang@users.noreply.github.com>
2019-05-03 06:58:22 +0900

    Update views.py

--- CRUD/blog/views.py
+++ CRUD/blog/views.py
@@ -7,4 +7,5 @@ def layout(request):
     return render(request, 'blog/layout.html')
 
 def home(request):
-    return render(request, 'blog/home.html')
+    blogs = Blog.objects
+    return render(request, 'blog/home.html', {'blogs': blogs})
