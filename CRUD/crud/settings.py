8eaec0fb04976f8501c18b7fdc6fa346f7c07e1a
SinHaecheol <since19990820@gmail.com>
2019-05-12 16:31:15 +0900

    1

--- CRUD/crud/settings.py
+++ CRUD/crud/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'blog', 'static'),
+    os.path.join(BASE_DIR, 'blog', 'static', 'blog'),
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
